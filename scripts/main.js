3b529c7ecc585e94eebe32b8861c5219aa27b6aa
kp-void <contact@cody-p.me>
2020-04-07 22:39:43 -0700

    overhaul of navigation system

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,27 @@ function page_load() {
     var request = new XMLHttpRequest();
     set_iframe(page);
 }
+
+function resizeIframe() {
+    frame_obj = document.getElementById("content");
+    var frame_url = "/" + frame_obj.contentWindow.location.href.replace(/^(?:\/\/|[^/]+)*\//, '');
+
+    if (frame_url != "/blank.html") // when the page first loads the dummy src is put there
+    {
+        frame_obj.style.height = frame_obj.contentWindow.document.documentElement.scrollHeight + 'px';
+        frame_obj.style.width = frame_obj.contentWindow.document.documentElement.scrollWidth + 'px';
+        window.history.replaceState({}, "Page Title", "/?page=" + frame_url);
+    };
+}
+
+function set_iframe(page_url) {
+    var fr = document.getElementById("content");
+    if (fr == null)
+    {
+        location.href = page_url;
+    }
+    else
+    {
+        fr.src = page_url;
+    }
+}
